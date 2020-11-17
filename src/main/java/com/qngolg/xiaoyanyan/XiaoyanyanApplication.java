2bee263a921db36154be8dc1bf5b2d5d84b197b6
qgl <qgl@qgls-MacBook-Pro.local>
2020-11-17 23:01:52 +0800

    first commit

---
+++ src/main/java/com/qngolg/xiaoyanyan/XiaoyanyanApplication.java
@@ -0,0 +1,13 @@
+package com.qngolg.xiaoyanyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoyanyanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoyanyanApplication.class, args);
+    }
+
+}

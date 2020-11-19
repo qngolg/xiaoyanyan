5d92b867940b8e9aa1a6e28c6cb5056666270dba
qgl <qngolg@126.com>
2020-11-19 21:05:49 +0800

    编写集成测试

--- src/main/java/com/qngolg/xiaoyanyan/repository/po/UserPo.java
+++ src/main/java/com/qngolg/xiaoyanyan/repository/po/UserPo.java
@@ -1,15 +1,19 @@
 package com.qngolg.xiaoyanyan.repository.po;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "user")
-@Data
 public class UserPo {
     @Id
     private Long id;
---
+++ src/test/java/com/qngolg/xiaoyanyan/controller/UserControllerTest.groovy
@@ -0,0 +1,36 @@
+package com.qngolg.xiaoyanyan.controller
+
+import com.qngolg.xiaoyanyan.repository.po.UserPo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import spock.lang.Specification
+
+import javax.persistence.EntityManager
+import java.time.LocalDate
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+
+@SpringBootTest
+class UserControllerTest extends Specification {
+    @Autowired
+    MockMvc mockMvc
+
+    @Autowired
+    EntityManager entityManager
+
+    void "should get a user"(){
+        given:
+        def userPo = UserPo.builder().id(2).name("peiqi").age(1).birthDay(LocalDate.now()).build()
+        entityManager.persist(userPo)
+
+        when:
+        def result = mockMvc.perform(get("/user/2"))
+
+        then:
+        result.andExpect(jsonPath('$.name', MockMvcResultMatchers.equals('peiqi')))
+    }
+}
